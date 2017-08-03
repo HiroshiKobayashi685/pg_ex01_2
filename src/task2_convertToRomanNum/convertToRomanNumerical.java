0764d026d3d3bd1cf0be30922681518b3aa0514e
koba58 <hiroshi5@10.34.41.13>
2017-08-03 22:58:27 +0900

    メソッドで実行できるように修正

--- src/task2_convertToRomanNum/convertToRomanNumerical.java
+++ src/task2_convertToRomanNum/convertToRomanNumerical.java
@@ -2,60 +2,58 @@ package task2_convertToRomanNum;
 
 public class convertToRomanNumerical {
 
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+    public static void main(String[] args) {
+        // TODO 自動生成されたメソッド・スタブ
 
-		int i = 19;
+    	int i = 0;
+    	while(i <= 21){
+    		convert(i);
+    		i++;
+    	}
 
-		int MIN_VAL=1;
-		int MAX_VAL=20;
+    }
 
-		if( i<MIN_VAL || i > MAX_VAL){
-			System.out.println("変換できませんでした");
-			System.out.println(MIN_VAL+"から"+MAX_VAL+"までの値を入力してください");
-			return ;
-		}
+    public static void convert(int i) {
 
-		else{
+        //上下限を以下に指定
+        int MIN_VAL=1;
+        int MAX_VAL=20;
 
-			while(i!=0){
+        if( i<MIN_VAL || i > MAX_VAL){
+            System.out.println("変換できませんでした");
+            System.out.println(MIN_VAL+"から"+MAX_VAL+"までの値を入力してください");
+            return ;
+        }
 
-				/*
-				String[][] mapRule = {{"10","X"},{"9","IX"},{"5","V"},{"4","IV"},{"1","I"}};
+        else{
 
-				int count = 0;
+            while(i!=0){
 
-				while(count<mapRule.length){
+                if(i>=10){
+                    System.out.print("X");
+                    i = i - 10;
+                }
+                else if (i==9){
+                    System.out.print("IX");
+                    i = i - 9;
+                }
+                else if (i>=5){
+                    System.out.print("V");
+                    i = i - 5;
+                }
+                else if (i==4){
+                    System.out.print("IV");
+                    i = i - 4;
+                }
+                else{
+                    System.out.print("I");
+                    i = i - 1;
+                }
+            }
+            System.out.println("");
+            return ;
+        }
 
-					if
-
-					count++;
-				}
-				*/
-				if(i>=10){
-					System.out.print("X");
-					i = i - 10;
-				}
-				else if (i==9){
-					System.out.print("IX");
-					i = i - 9;
-				}
-				else if (i>=5){
-					System.out.print("V");
-					i = i - 5;
-				}
-				else if (i==4){
-					System.out.print("IV");
-					i = i - 4;
-				}
-				else{
-					System.out.print("I");
-					i = i - 1;
-				}
-			}
-			return ;
-		}
-
-	}
+    }
 
 }
